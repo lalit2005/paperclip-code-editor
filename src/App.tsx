62bf5dd3d23aa82f4b3d8e2f44f66b410c93d775
Lalit <lalitvijay9480@gmail.com>
2021-08-25 23:16:12 +0530

    fix: file saving on pressing hotkey

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Editor from "@monaco-editor/react";
 import * as Tabs from "@radix-ui/react-tabs";
 // @ts-ignore
 import ReactSrcDocIframe from "react-srcdoc-iframe";
+import * as Popover from "@radix-ui/react-popover";
 
 function App() {
   const [tab, setTab] = useState<"html" | "css" | "js">("html");
@@ -24,16 +25,8 @@ function App() {
       // if cmd/ctrl + s is pressed, console log Saved
       if (e.key === "s" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
-        window.top.postMessage(
-          {
-            sentToPaperclip: true,
-            saveCode: true,
-            htmlCode: htmlCode,
-            cssCode: cssCode,
-            jsCode: jsCode,
-          },
-          "*"
-        );
+        // @ts-ignore
+        document.querySelector("#save-btn").click();
       }
       // if cmd/ctrl + 1 is pressed, set tab to "html"
       if (e.key === "1" && (e.metaKey || e.ctrlKey)) {
@@ -146,6 +139,37 @@ function App() {
                     title="Cmd/Ctrl + S">
                     Save
                   </button>
+                </div>
+                <div>
+                  <Popover.Root>
+                    <Popover.Trigger>
+                      <button
+                        id="save-btn"
+                        className="px-3 py-1 my-2 border border-gray-500 rounded cursor-pointer bg-gray-50 hover:bg-gray-100 "
+                        title="More options">
+                        <svg
+                          width="15"
+                          height="15"
+                          viewBox="0 0 15 15"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            d="M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z"
+                            fill="currentColor"
+                            fillRule="evenodd"
+                            clipRule="evenodd"></path>
+                        </svg>
+                      </button>
+                    </Popover.Trigger>
+                    <Popover.Anchor />
+                    <Popover.Content>
+                      <div className="px-5 py-2 bg-white border border-gray-600 rounded shadow-xl">
+                        Hello 👋
+                      </div>
+                      <Popover.Close />
+                      <Popover.Arrow />
+                    </Popover.Content>
+                  </Popover.Root>
                 </div>
               </div>
             </Tabs.List>
